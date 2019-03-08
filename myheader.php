48aa4ca5f153a4b81bbf2d776dcc361d81e55852
c33luk3 <46679824+c33luk3@users.noreply.github.com>
2019-03-08 15:22:20 -0500

    Add files via upload

--- myheader.php
+++ myheader.php
@@ -3,22 +3,50 @@
 ?>
 <style>
 h1{
-    color: red;
-    font-style: italic;
-    font-size:30px;
+    color:black;
+    font-style:italic;
+    font-size:60px;
 }
 html{
     text-align:center;
     font-style:italic;
-    font-size:30px;
+    font-size:35px;
+    background-color:black;
     color:red;
+    margin-top:-10px;
 }
 form{
-    color: blue;
+    color: green;
     font-style: italic;
-    font-size:30px;
+    font-size:40px;
     text-align:center;
+    margin-top:-15px;
 }
+.topnav a {
+    color:yellow;
+}
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color:red;
+  color:black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color:black;
+  color: blue;
+}
+.img {
+    padding-top:-20px;
+    margin-top:-30px;
+}
+input {
+    color:black;
+    font-size:20px;
+    background-color:white;
+    border-color:black;
+} 
+
 </style>
 </body>
 </html>
